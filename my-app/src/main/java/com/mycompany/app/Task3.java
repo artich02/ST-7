b81495df7972fca67f278866ba03d1f5fee645bb
artich02 <tar8803@gmail.com>
2025-05-31 16:47:33 +0300

    update

--- my-app/src/main/java/com/mycompany/app/Task3.java
+++ my-app/src/main/java/com/mycompany/app/Task3.java
@@ -9,65 +9,88 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.io.FileWriter;
+import java.io.IOException;
 import java.io.PrintWriter;
 
-public class Task3 {
-    public static void getWeatherForecast() {
-        WebDriver webDriver = new ChromeDriver();
+public class WeatherForecastFetcher {
+    
+    private static final String WEATHER_API_URL = 
+        "https://api.open-meteo.com/v1/forecast?latitude=56&longitude=44" +
+        "&hourly=temperature_2m,rain&current=cloud_cover" +
+        "&timezone=Europe%2FMoscow&forecast_days=1&wind_speed_unit=ms";
+    
+    private static final String OUTPUT_FILE = "result/forecast.txt";
+    
+    public static void fetchWeatherData() {
+        WebDriver driver = initializeDriver();
         try {
-            String url = "https://api.open-meteo.com/v1/forecast?latitude=56&longitude=44&hourly=temperature_2m,rain&current=cloud_cover&timezone=Europe%2FMoscow&forecast_days=1&wind_speed_unit=ms";
-            webDriver.get(url);
-
-            // Получаем элемент с JSON-данными
-            WebElement element = webDriver.findElement(By.tagName("pre"));
-            String jsonStr = element.getText();
-
-            // Парсим JSON
-            JSONParser parser = new JSONParser();
-            JSONObject obj = (JSONObject) parser.parse(jsonStr);
-
-            // Извлекаем необходимые данные
-            JSONObject hourly = (JSONObject) obj.get("hourly");
-            JSONArray timeArray = (JSONArray) hourly.get("time");
-            JSONArray temperatureArray = (JSONArray) hourly.get("temperature_2m");
-            JSONArray rainArray = (JSONArray) hourly.get("rain");
-
-            // Открываем файл для записи результатов
-            PrintWriter writer = new PrintWriter(new FileWriter("result/forecast.txt"));
-
-            // Выводим заголовок таблицы
-            String header = String.format("| %-3s | %-18s | %-11s | %-12s |", "№", "Дата/время", "Температура", "Осадки (мм)");
-            String separator = "|-----|--------------------|-------------|--------------|";
-
-            System.out.println(header);
-            System.out.println(separator);
-
-            writer.println(header);
-            writer.println(separator);
-
-            // Заполняем таблицу данными
-            for (int i = 0; i < timeArray.size(); i++) {
-                String time = (String) timeArray.get(i);
-                Double temperature = Double.parseDouble(temperatureArray.get(i).toString());
-                Double rain = Double.parseDouble(rainArray.get(i).toString());
-
-                String row = String.format("| %-3d | %-18s | %-11.1f | %-12.2f |",
-                        (i + 1), time, temperature, rain);
-
-                System.out.println(row);
-                writer.println(row);
-            }
-
-            writer.close();
-            System.out.println("\nРезультаты сохранены в файл result/forecast.txt");
-
-            Thread.sleep(3000); // Пауза для возможности увидеть результат
-
+            processWeatherData(driver);
         } catch (Exception e) {
-            System.out.println("Ошибка при получении прогноза погоды:");
-            System.out.println(e.toString());
+            handleError(e);
         } finally {
-            webDriver.quit();
+            driver.quit();
         }
+    }
+    
+    private static WebDriver initializeDriver() {
+        return new ChromeDriver();
+    }
+    
+    private static void processWeatherData(WebDriver driver) throws Exception {
+        driver.get(WEATHER_API_URL);
+        String jsonData = extractJsonData(driver);
+        JSONObject parsedData = parseJsonData(jsonData);
+        writeWeatherReport(parsedData);
+        Thread.sleep(3000);
+    }
+    
+    private static String extractJsonData(WebDriver driver) {
+        WebElement jsonElement = driver.findElement(By.tagName("pre"));
+        return jsonElement.getText();
+    }
+    
+    private static JSONObject parseJsonData(String jsonStr) throws Exception {
+        JSONParser jsonParser = new JSONParser();
+        return (JSONObject) jsonParser.parse(jsonStr);
+    }
+    
+    private static void writeWeatherReport(JSONObject weatherData) throws IOException {
+        JSONObject hourlyData = (JSONObject) weatherData.get("hourly");
+        JSONArray times = (JSONArray) hourlyData.get("time");
+        JSONArray temperatures = (JSONArray) hourlyData.get("temperature_2m");
+        JSONArray rainfall = (JSONArray) hourlyData.get("rain");
+        
+        try (PrintWriter outputWriter = new PrintWriter(new FileWriter(OUTPUT_FILE))) {
+            printWeatherTable(times, temperatures, rainfall, outputWriter);
+        }
+    }
+    
+    private static void printWeatherTable(JSONArray times, JSONArray temperatures, 
+                                        JSONArray rainfall, PrintWriter writer) {
+        String tableHeader = String.format("| %-3s | %-18s | %-11s | %-12s |", 
+                                         "№", "Дата/время", "Температура", "Осадки (мм)");
+        String tableDivider = "|-----|--------------------|-------------|--------------|";
+        
+        System.out.println(tableHeader);
+        System.out.println(tableDivider);
+        writer.println(tableHeader);
+        writer.println(tableDivider);
+        
+        for (int index = 0; index < times.size(); index++) {
+            String timestamp = (String) times.get(index);
+            double temp = Double.parseDouble(temperatures.get(index).toString());
+            double precip = Double.parseDouble(rainfall.get(index).toString());
+            
+            String tableRow = String.format("| %-3d | %-18s | %-11.1f | %-12.2f |",
+                                          (index + 1), timestamp, temp, precip);
+            
+            System.out.println(tableRow);
+            writer.println(tableRow);
+        }
+    }
+    
+    private static void handleError(Exception error) {
+        System.err.println("Произошла ошибка при получении данных о погоде:");
+        error.printStackTrace();
     }
 }
